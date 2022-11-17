b18bf34ddc869b24121d0cfea8cb2500aecd4507
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2022-11-18 00:17:18 +0200

    add customer Ticket class

---
+++ src/CustomerTicket.java
@@ -0,0 +1,13 @@
+public class CustomerTicket {
+    String name;
+    int id;
+    String ticketContent;
+
+    public CustomerTicket(String name, int id, String content)
+    {
+        this.name = name;
+        this.id = id;
+        this.ticketContent = content;
+    }
+
+}
